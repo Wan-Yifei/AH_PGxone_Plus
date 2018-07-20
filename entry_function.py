ed7b6adbd46c5d929ea194eb4920398ece627fa0
YIfei Wan <ywan@localhost.localdomain>
2018-07-19 22:55:09 -0400

    Feat: A entry function
    This is a demo entry function which is use to accept argument from shell.

---
+++ entry_function.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 19 21:25:34 2018
+
+@author: yifei.wan
+"""
+
+import sys
+import getopt
+
+def pathways(args):
+    '''
+    This is a entry function. It is utilized to accept paths of input file 
+    and out file from shell respectively. It also provides options for 
+    linux commands.
+    '''
+    inputfile = ''
+    outputfile = ''
+    
+    try:
+        opts, arg = getopt.getopt(args, 'hi:o:')
+    except getopt.GetoptError:
+        print('Usage: -i <path of input> -o <path of output>')
+        sys.exit(2)
+        
+    for opt, arg in opts:
+        if opt == '-h':
+            print('Usage: PGxOneV3_Drug_extract.py -i <path of input> -o <path of output>')
+            sys.exit(0)
+        elif opt == '-i':
+            inputfile = arg
+        elif opt == '-o':
+            outputfile = arg
+    
+    print('input %s' %inputfile)
+    print('outputfile %s' %outputfile)
+    
+    
+pathways(sys.argv[1:])            
+            
+    
