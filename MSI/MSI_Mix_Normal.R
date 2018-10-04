610f949c9af8c4b8537ab12228f5e3d02566b941
Wan-Yifei <wanyifei0123@126.com>
2018-10-04 18:32:33 -0400

    Refactor: Extract Method and Encapsulation
    Encapsulate functions and remove right tail cutoff.

--- MSI/MSI_Mix_Normal.R
+++ MSI/MSI_Mix_Normal.R
@@ -3,16 +3,20 @@
 # 1. Input data
 setwd('c:/Users/yifei.wan/Desktop')
 #count_raw = read.csv('All_counts.txt', sep = '\t', header = FALSE, stringsAsFactors = FALSE)
-count_raw = read.csv('Msi_raw_count/1_percent_LoVo_Large-RD_S37_msi_array.txt_counts.txt', sep = '\t', header = FALSE, stringsAsFactors = FALSE)
+#count_raw = read.csv('Msi_raw_count/1_percent_LoVo_Large-RD_S37_msi_array.txt_counts.txt', sep = '\t', header = FALSE, stringsAsFactors = FALSE)
+count_raw = read.csv('Msi_raw_count/1_percent_HCT16-Large-RD_S39_msi_array.txt', sep = '\t', header = FALSE, stringsAsFactors = FALSE)
 
-# 2. Summary
-library(ggplot2)
-count_raw <- t(count_raw)
-colnames(count_raw) <- count_raw[1,]
-count_raw <- as.data.frame(count_raw[-1, ])
-count_raw <- data.frame(sapply(count_raw, as.character), stringsAsFactors = FALSE)
-count_raw <- data.frame(sapply(count_raw, as.integer))
-qplot(x = count_raw$MSI_LEN_BAT.26, y = count_raw$MSI_FREQ_BAT.26)
+# 2. Prepare structure
+prestr <- function(count_raw){
+  count_raw <- t(count_raw)
+  colnames(count_raw) <- count_raw[1,]
+  count_raw <- as.data.frame(count_raw[-1, ])
+  count_raw <- data.frame(sapply(count_raw, as.character), stringsAsFactors = FALSE)
+  count_raw <- data.frame(sapply(count_raw, as.integer))
+  return(count_raw)
+}
+
+count_raw <- prestr(count_raw)
 
 # 3. Normalization
 # Normalize <- function(count_raw){
@@ -38,7 +42,8 @@ library(plyr)
 freque <- function(raw_count, mark){
   count_rep = data.frame()
   raw_count[is.na(raw_count)] = 0
-  for (i in 1:15){
+  #for (i in 1:dim(raw_count)[1]){
+  for (i in 1:dim(count_raw[1])){
     row = data.frame(rep(raw_count[i, mark], raw_count[i, mark + 1]))
     count_rep = rbind(count_rep, row)
   }
@@ -46,15 +51,18 @@ freque <- function(raw_count, mark){
   return(count_rep)
 }
 
-for(i in 1:10){
+prerep <- function(count_raw){
+  for(i in 1:10){
   if(i%%2 != 0){
-    assign(substr(colnames(count_raw)[i],9, nchar(colnames(count_raw)[i])), freque(count_raw, i))
+    assign(substr(colnames(count_raw)[i],9, nchar(colnames(count_raw)[i])), freque(count_raw, i), envir = .GlobalEnv)
+    }
   }
 }
 
+prerep(count_raw)
+
 # 5. Mixture Normal Model
 library(mclust)
-set.seed(121)
 
 
 # fit25 <- densityMclust(BAT.25[, 1], G =2, model = 'V')
@@ -64,16 +72,26 @@ set.seed(121)
 # hist(BAT.25[,1], xlab = 'Length', ylab = '', main = 'Density of BAT.25')
 # #plot(fit25, what = 'BIC')
 
+set.seed(121)
 ModelFit <- function(mark, nsubdis = 2){
+
   fit <- densityMclust(mark[, 1], G = nsubdis, model = 'V')
+  print(colnames(mark))
   print(summary(fit,parameters = T))
-  plot(fit, what = 'density', xaxt = 'n', yaxt = 'n', xlab = '', main = '')
+  barplot(table(mark), xlab = 'Length', ylab = '', main = paste('Density of ', colnames(mark)))  
   par(new = TRUE)
-  hist(mark[,1], xlab = 'Length', ylab = '', main = paste('Density of ', colnames(mark)))
+  plot(fit, what = 'density', xaxt = 'n', yaxt = 'n', xlab = '', main = '')
 }
 
-ModelFit(BAT.25)
-ModelFit(BAT.26, nsubdis = 2)
-ModelFit(NR.21)
-ModelFit(NR.24, nsubdis = 2)
-ModelFit(NR.27)
+MSI_modelfit_main <- function(count_raw){
+  set.seed(121)
+  count_raw <- prestr(count_raw)
+  prerep
+  fit_BAT.25 = ModelFit(BAT.25)
+  fit_BAT.26 = ModelFit(BAT.26, nsubdis = 2)
+  fit_NR.21 = ModelFit(NR.21, nsubdis = 2)
+  fit_NR.24 = ModelFit(NR.24, nsubdis = 2)
+  fit_NR.27 = ModelFit(NR.27, nsubdis = 2)
+}
+
+MSI_modelfit_main(count_raw)
