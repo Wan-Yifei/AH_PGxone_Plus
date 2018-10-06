a9299c5d15d87e3977c40c1340ab9d062f8492fd
Wan-Yifei <wanyifei0123@126.com>
2018-10-06 18:34:32 -0400

    Refactor: Encapsulate functions
    Encapsulate all functions and add break check function.

--- MSI/MSI_Mix_Normal_batch.R
+++ MSI/MSI_Mix_Normal_batch.R
@@ -4,10 +4,10 @@
 setwd('c:/Users/yifei.wan/Desktop')
 #count_raw = read.csv('All_counts.txt', sep = '\t', header = FALSE, stringsAsFactors = FALSE)
 #count_raw = read.csv('Msi_raw_count/1_percent_LoVo_Large-RD_S37_msi_array.txt_counts.txt', sep = '\t', header = FALSE, stringsAsFactors = FALSE)
-#count_raw <- read.csv('MSI_positive/1_percent_HCT116_Large-RD_S20_msi_array.txt', sep = '\t', header = FALSE, stringsAsFactors = FALSE)
+count_raw <- read.csv('MSI_positive/1_percent_HCT116_Large-RD_S20_msi_array.txt', sep = '\t', header = FALSE, stringsAsFactors = FALSE)
 
 path_pos <- 'c:/Users/yifei.wan/Desktop/MSI_positive'
-path_neg <- 'c:/Users/yifei.wan/Desktop/MSI_healthy'
+path_neg <- 'c:/Users/yifei.wan/Desktop/MSI_negative'
 
 input_file <- function(path_way){
   files = list.files(path_way, pattern = '*.txt')
@@ -17,7 +17,12 @@ input_file <- function(path_way){
 
 count_all <- input_file(path_pos)
 
-# 2. Prepare structure
+########################################################################################
+##################################### Functions ########################################
+########################################################################################
+
+# 1. Prepare structure
+
 prestr <- function(count_raw){
   count_raw <- t(count_raw)
   colnames(count_raw) <- count_raw[1,]
@@ -27,9 +32,72 @@ prestr <- function(count_raw){
   return(count_raw)
 }
 
-count_raw <- prestr(count_raw)
 
-# 3. Normalization
+# 2. Replicate
+
+## 2.1 Transform frequence to original discrete data
+freque <- function(raw_count, mark){
+  count_rep = data.frame()
+  raw_count[is.na(raw_count)] = 0
+  for (i in 1:dim(raw_count)[1]){
+    row = data.frame(rep(raw_count[i, mark], raw_count[i, mark + 1]))
+    count_rep = rbind(count_rep, row)
+  }
+  colnames(count_rep) = colnames(count_raw)[mark]
+  return(count_rep)
+}
+
+## 2.2 Cutoff function
+## detecte and remove length without count on right side
+
+breakcheck <- function(count_row){
+  breakpoints = which(sapply(count_row, function(x)(x+1) %in% count_row) == 0)
+  check = ifelse(count_row[breakpoints] > quantile(na.omit(count_row))[3], 1, 0)
+  cutpoint = breakpoints[min(which(check == 1))]
+  count_row[-c(1:cutpoint)] = NA
+  return(count_row)
+}
+
+## 2.3 Apply freque to odd columns (length of each mark)
+prerep <- function(count_raw){
+  for(i in 1:10){
+    if(i%%2 != 0){
+      count_mark = freque(count_raw, i)
+      count_mark[which(count_mark == 0), ] = NA
+      assign(substr(colnames(count_raw)[i],9, nchar(colnames(count_raw)[i])), count_mark, envir = .GlobalEnv)
+    }
+  }
+}
+
+# 3. Mixture Normal Model
+
+library(mclust)
+ModelFit <- function(mark, nsubdis = 2){
+  
+  colnames(mark) = paste('Mixture Gauss model of', substr(deparse(substitute(mark)),9,nchar(deparse(substitute(mark))) - 1))
+  fit = densityMclust(mark[, 1], G = nsubdis, model = 'V')
+  print(colnames(mark))
+  print(summary(fit,parameters = T)[10:12])
+  plot(table(mark), xlab = 'Length', ylab = '', main = paste('Density of ', colnames(mark)))
+  #barplot(table(mark), xlab = 'Length', ylab = '', main = paste('Density of ', colnames(mark)))
+  par(new = TRUE)
+  plot(fit, what = 'density', xaxt = 'n', yaxt = 'n', xlab = '', main = '')
+}
+
+MSI_modelfit_main <- function(count_raw){
+  set.seed(121)
+  count_raw = prestr(count_raw)
+  count_raw[, seq(1, 9, 2)] = as.data.frame(sapply(count_raw[, seq(1, 9, 2)], breakcheck))
+  prerep(count_raw)
+  
+  fit_BAT.25 = ModelFit(na.omit(BAT.25))
+  fit_BAT.26 = ModelFit(na.omit(BAT.26), nsubdis = 2)
+  fit_NR.21 = ModelFit(na.omit(NR.21), nsubdis = 2)
+  fit_NR.24 = ModelFit(na.omit(NR.24), nsubdis = 2)
+  fit_NR.27 = ModelFit(na.omit(NR.27), nsubdis = 2)
+}
+
+# 4. Normalization
 # Normalize <- function(count_raw){
 #   tmp <- count_raw
 #   index <- c(2, 4, 6, 8, 10)
@@ -47,62 +115,6 @@ count_raw <- prestr(count_raw)
 # 
 # count_normalized <- Normalize(count_raw)
 
-# 4. Replicate
-library(plyr)
 
-freque <- function(raw_count, mark){
-  count_rep = data.frame()
-  raw_count[is.na(raw_count)] = 0
-  #for (i in 1:dim(raw_count)[1]){
-  for (i in 1:dim(count_raw[1])){
-    row = data.frame(rep(raw_count[i, mark], raw_count[i, mark + 1]))
-    count_rep = rbind(count_rep, row)
-  }
-  colnames(count_rep) = colnames(count_raw)[mark]
-  return(count_rep)
-}
-
-prerep <- function(count_raw){
-  for(i in 1:10){
-  if(i%%2 != 0){
-    assign(substr(colnames(count_raw)[i],9, nchar(colnames(count_raw)[i])), freque(count_raw, i), envir = .GlobalEnv)
-    }
-  }
-}
-
-prerep(count_raw)
-
-# 5. Mixture Normal Model
-library(mclust)
-
-
-# fit25 <- densityMclust(BAT.25[, 1], G =2, model = 'V')
-# summary(fit25,parameters=T)
-# plot(fit25, what = 'density', xaxt = 'n', yaxt = 'n', xlab = '', main = '')
-# par(new = TRUE)
-# hist(BAT.25[,1], xlab = 'Length', ylab = '', main = 'Density of BAT.25')
-# #plot(fit25, what = 'BIC')
-
-set.seed(121)
-ModelFit <- function(mark, nsubdis = 2){
-
-  fit <- densityMclust(mark[, 1], G = nsubdis, model = 'V')
-  print(colnames(mark))
-  print(summary(fit,parameters = T))
-  barplot(table(mark), xlab = 'Length', ylab = '', main = paste('Density of ', colnames(mark)))  
-  par(new = TRUE)
-  plot(fit, what = 'density', xaxt = 'n', yaxt = 'n', xlab = '', main = '')
-}
-
-MSI_modelfit_main <- function(count_raw){
-  set.seed(121)
-  count_raw <- prestr(count_raw)
-  prerep
-  fit_BAT.25 = ModelFit(BAT.25)
-  fit_BAT.26 = ModelFit(BAT.26, nsubdis = 2)
-  fit_NR.21 = ModelFit(NR.21, nsubdis = 2)
-  fit_NR.24 = ModelFit(NR.24, nsubdis = 2)
-  fit_NR.27 = ModelFit(NR.27, nsubdis = 2)
-}
 
 MSI_modelfit_main(count_raw)
