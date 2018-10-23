0d3a97607286365a2454231d30a5adbcd06296c2
Wan-Yifei <wanyifei0123@126.com>
2018-10-23 18:40:13 -0400

    Fix: Update the skewness
    Update the skewness to avoid the error: out of index.

--- MSI/MSI_noise_generator.R
+++ MSI/MSI_noise_generator.R
@@ -5,12 +5,12 @@
 ###############################################################
 
 
-Random_signal <- function(scale = 15, skewness = sample(1:scale, 1), intensity = 0.03, count = 10000){
+Random_signal <- function(scale = 10, skewness = sample(1:(scale - 1), 1), intensity = 0.03, count = 10000){
   seeds <- runif(scale, 0, 1)
   signal_pool <- seeds/sum(seeds)
   signal_max <- max(signal_pool)
   signal_tails <- signal_pool[-which(signal_pool == signal_max)]
-  group_index <- sample(1:scale - 1, skewness)
+  group_index <- sample(2:scale - 1, skewness)
   signal_tail_right <- sort(signal_tails[group_index], decreasing = T)
   signal_tail_left <- sort(signal_tails[-group_index])
   signal_pro <- c(signal_tail_left, signal_max, signal_tail_right)
@@ -22,5 +22,5 @@ Random_signal <- function(scale = 15, skewness = sample(1:scale, 1), intensity =
   return(list(signal_count_scaled))
 }
 
-Random_signal()
+Random_signal(skewness = 8)
 
