afadc483606803348d0468041d38c9fab73f87db
Wan-Yifei <yifei.wan@admerahealth.com>
2019-01-16 18:57:01 -0500

    Feat: Find path baased on ID
    Find the correposnding run folder based on ID.

--- Amend_report/Amend_list_Check.sh
+++ Amend_report/Amend_list_Check.sh
@@ -1,6 +1,11 @@
 #! /bin/bash
 
-cat $1 | while read line 
+awk '{if($1~/A-*/){print $1,$2,$3}}' $1 | while read ID type Med ICD
 do
-	awk '{if($1~/A-*/){print $0}}'
+	find /data/CLIA-Data/PGxOne_V3/Production/BI_Data_Analysis/ -name "$ID*.vcf" | while read path
+	do
+		dirname $path >> tmp.txt
+	done
+	$(sort -u -t_ -k4 tmp.txt) | echo
+	echo "$ID"
 done
