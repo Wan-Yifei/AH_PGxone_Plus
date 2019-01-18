f1d10e51ce2d1a8d98ae8daf567ad59e5cfc9298
Wan-Yifei <yifei.wan@admerahealth.com>
2019-01-17 19:11:56 -0500

    Feat: Extract Run folder index and set reponse information

--- Amend_report/Amend_list_Check.sh
+++ Amend_report/Amend_list_Check.sh
@@ -1,11 +1,19 @@
 #! /bin/bash
 
+## Read amend requirement from require list
 awk '{if($1~/A-*/){print $1,$2,$3}}' $1 | while read ID type Med ICD
 do
+## Find run folder based on ID
 	find /data/CLIA-Data/PGxOne_V3/Production/BI_Data_Analysis/ -name "$ID*.vcf" | while read path
 	do
 		dirname $path >> tmp.txt
 	done
-	$(sort -u -t_ -k4 tmp.txt) | echo
-	echo "$ID"
+	runfolder=$(sort -u -t_ -rk4 tmp.txt | head -n 1 | cut -d / -f7)
+	run_index=$(echo $runfolder | cut -d _ -f4)
+	echo Amend $ID from $run_index 
+	#python3 PGx_report_amend.py $runfolder $type $Med $ICD
+	if [ $type = "Medication" ];
+	then
+		echo $ID from $run_index has been updated $type!
+	fi
 done
