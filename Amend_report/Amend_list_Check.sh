e6fc89faf09c641619c81ae0301eb8882498d1e9
Wan-Yifei <yifei.wan@admerahealth.com>
2019-01-22 18:10:19 -0500

    Feat: E-mail reminder
    After update required sample report, the script would send an e-mail to reminder BI staff and save the sample info to log.

--- Amend_report/Amend_list_Check.sh
+++ Amend_report/Amend_list_Check.sh
@@ -1,4 +1,7 @@
 #! /bin/bash
+set -e
+## message for Lab director
+MESSAGE="Please resign with \"Update"
 
 ## Read amend requirement from require list
 awk '{if($1~/A-*/){print $1,$2,$3}}' $1 | while read ID TYPE MED ICD
@@ -15,13 +18,14 @@ do
 	#python3 PGx_report_amend.py $runfolder $TYPE $MED $ICD
 	if [ $TYPE = "Medication" ]
 	then
-		echo The $TYPE list of $ID from $run_index has been updated!
+		echo `date`	The $TYPE list of $ID from $run_index has been updated! $MESSAGE $TYPE\". | tee Amend_log.txt | mail -s "Pleas resign $ID" yifei.wan@admerahealth.com 
 	elif [ $TYPE = "ICD" ]
 	then 
-		echo The $TYPE of $ID from $run_index has been updated!
+		echo `date`	The $TYPE codes of $ID from $run_index has been updated! $MESSAGE $TYPE\". | tee Amend_log.txt | mail -s "Pleas resign $ID" yifei.wan@admerahealth.com
 	elif [ $TYPE = "Both" ]
-		echo The ICD and Medication of $ID from $run_index have been updated!
+	then
+		echo `date`>The ICD and Medication of $ID from $run_index have been updated! $MESSAGE $TYPE\". | tee Amend_log.txt | mail -s "Pleas resign $ID" yifei.wan@admerahealth.com
 	else
-		echo $TYPE of $ID has been update. Please check!
+		echo `date`	$TYPE of $ID has been update. Please check! | tee Amend_log.txt | mail -s "Pleas check $ID" yifei.wan@admerahealth.com
 	fi
 done
