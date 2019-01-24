e3ea52e93a70e97154c75d1fef16c88644df19bd
Wan-Yifei <yifei.wan@admerahealth.com>
2019-01-23 23:21:41 -0500

    Feat: Run py and bash script to amend action file

--- Amend_report/Amend_list_Check.sh
+++ Amend_report/Amend_list_Check.sh
@@ -14,12 +14,19 @@ do
 	runfolder=$(sort -u -t_ -rk4 tmp.txt | head -n 1 | cut -d / -f7) ## pathway of run folder
 	run_index=$(echo $runfolder | cut -d _ -f4) ## e.g. Run700
 	rm tmp.txt ## delet tmp
+	echo -e
+	echo ================================================================
+	echo ================================================================
+	echo -e
 	echo Amend $ID from $run_index 
+	echo -e
+
 ## Amend report
-	#python3 PGx_report_amend.py $runfolder $TYPE $MED $ICD
+	python3 PGx_report_amend.py $runfolder $ID $TYPE -M $MED -I $ICD
+	bash /home/yifei.wan/AH_PGxOne_Plus/PGx_Run/PGxOne_Scripts.sh $runfolder
 	if [ $TYPE = "Medication" ]
 	then
-		echo `date`	The $TYPE list of $ID from $run_index has been updated! $MESSAGE $TYPE\". | tee Amend_log.txt | mail -s "Pleas resign $ID" yifei.wan@admerahealth.com 
+		echo `date`	The $TYPE list of $ID from $run_index has been updated! $MESSAGE $TYPE\". | tee Amend_log.txt | mail -s "Pleas resign $ID" yifei.wan@admerahealth.com zhuosheng.gu@admerahealth.com 
 	elif [ $TYPE = "ICD" ]
 	then 
 		echo `date`	The $TYPE codes of $ID from $run_index has been updated! $MESSAGE $TYPE\". | tee Amend_log.txt | mail -s "Pleas resign $ID" yifei.wan@admerahealth.com
