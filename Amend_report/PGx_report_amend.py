8a89cc56f9cce2d0d0984723b3b9e97bc78add3a
Wan-Yifei <yifei.wan@admerahealth.com>
2019-01-29 17:38:11 -0500

    Doc: Update introduction

--- Amend_report/PGx_report_amend.py
+++ Amend_report/PGx_report_amend.py
@@ -3,7 +3,7 @@
 # ================================================================================================
 # 01/29/2019	Beat version 0.0.1
 # Summary:
-# The Python3 script PGx_report_amend.py updates sample information for each sample. 
+# The Python3 script PGx_report_amend.py updates sample information in accession file for each sample. 
 # 
 # Input: See ParseArg() function and find more details at the top of script: Amend_list_check.sh.
 # Output: new sample codes_drugs_accession.txt for required samples.
