b2be3bfa9b5e79faf8baa7828ffd0e5a8bcf24b8
Wan-Yifei <yifei.wan@admerahealth.com>
2019-01-29 14:12:43 -0500

    Doc: Add introduction of this script

--- Amend_report/Amend_list_Check.sh
+++ Amend_report/Amend_list_Check.sh
@@ -1,5 +1,26 @@
 #! /bin/bash
-#To do: move new action file to batch uploading folder 
+
+# @author Yifei Wan
+
+# ===================================================================================================
+# 01/29/2019	Beta version 0.0.1
+# Summary:
+# The bash script Amend_list_check.sh is use to automatically amend PGxOne reports.
+#
+# ** Input files**
+# 1. Amend_request.txt: Accessioners input sample ID, amending content type and new added content.
+#	1.1 For ICD: Input all ICD codes the sample having;
+#	1.2 For MED: Input new medications or all medications of the sample, and use comma as delimiter;
+#	1.3 For TYPE: Which kind of content requires amending? ICD? Medication? Address...
+# 2. sample_codes_drugs.txt: Used to search CASE_ID of each sample.
+#
+# **Output files**
+# 1. Amend_log.txt: Record all processed samples;
+# 2. E-mail: This script would send reminder to Zhuosheng and Yifei.
+#
+# **Dependence**
+# PGx_report_amend.py	Beta version 0.0.1
+# ===================================================================================================
 
 set -e
 ## message for Lab director
@@ -13,7 +34,7 @@ do
 	do
 		dirname $path >> tmp.txt
 	done
-	runfolder=$(sort -u -t_ -rk4 tmp.txt | head -n 1 | cut -d / -f7) ## pathway of run folder
+	runfolder=$(sort -u -t_ -rk4 tmp.txt | head -n 1 | cut -d / -f7) ## pathway of run folder, select the newest folder
 	run_index=$(echo $runfolder | cut -d _ -f4) ## e.g. Run700
 	rm tmp.txt ## delet tmp
 	echo -e
