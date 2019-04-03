36dc53173e4ec88a0755beebb7e1c425ac8e5ec9
Wan-Yifei <yifei.wan@admerahealth.com>
2019-04-03 15:37:19 -0400

    Feat: Redirect std and err File: PGxOne_flatfile_scheduler.sh
    Redirect output and error to both console and file.
    Update to version number Basic 0.0.2.

--- PGx_App/PGxOne_flatfile_scheduler.sh
+++ PGx_App/PGxOne_flatfile_scheduler.sh
@@ -9,6 +9,11 @@
 # ** Input ** 
 # 1. the action files in the PGxOne_APP folder under the CLIA-Data disk.
 # ========================================================================
+# 04/03/2019	Basic verson 0.0.2
+# Redirect std and err
+#
+# Redirect standard output and error message to both file and console.
+# ========================================================================
 
 ACTIONFOLDER='/data/CLIA-Data/PGxOne_APP/'
 
@@ -30,7 +35,7 @@ fi
 
 
 Move_flat() {
-	cp -v $ACTIONFOLDER/*.txt /admeraappflatfilesdev/$1
+	cp -v $ACTIONFOLDER/*.txt /admeraappflatfilesdev/$1 > >(tee -a Bucket_log.txt) 2> >(tee -a Bucket_err_log.txt > &2)
 }
 
 
