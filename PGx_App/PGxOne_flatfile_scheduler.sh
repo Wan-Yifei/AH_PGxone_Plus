8dece12d99ed14ef3517c9c395c4112b4c1a3736
Wan-Yifei <yifei.wan@admerahealth.com>
2019-04-04 04:33:20 +0000

    Feat: Exit the script when input folder is empty File: PGxOne_flatfile_scheduler.sh
    Write a record to the log file and exit when input folder is empty.

--- PGx_App/PGxOne_flatfile_scheduler.sh
+++ PGx_App/PGxOne_flatfile_scheduler.sh
@@ -9,13 +9,26 @@
 # ** Input ** 
 # 1. the action files in the PGxOne_APP folder under the CLIA-Data disk.
 # ========================================================================
-# 04/03/2019	Basic verson 0.0.2
+# 04/03/2019	Basic version 0.0.2
 # Redirect std and err
 #
 # Redirect standard output and error message to both file and console.
 # ========================================================================
+# 04/03/2019	Basic version 0.0.3
+# Check sourse file
+#
+# When input folder is empty, write a line of record and exit.
+# ========================================================================
 
+set -e
 ACTIONFOLDER='/data/CLIA-Data/PGxOne_APP/'
+
+## If PGxOne_APP is empty, exit script
+if find "$ACTIONFOLDER" -maxdepth 0 -empty | grep -q "."
+then
+    echo "[$(date)] $ACTIONFOLDER is empty" >> /home/yifei.wan/yifei.wan/AH_Project/PGx_App/Bucket_log.txt    # Should this be >> to append?
+    exit 0
+fi
 
 ## current date
 date=$(date +%Y-%m-%d)
@@ -35,13 +48,15 @@ fi
 
 
 Move_flat() {
-	cp -v $ACTIONFOLDER/*.txt /admeraappflatfilesdev/$1 > >(tee -a Bucket_log.txt) 2> >(tee -a Bucket_err_log.txt > &2)
+	cp -v $ACTIONFOLDER/*.txt /admeraappflatfilesdev/$1 > >(tee -a /home/yifei.wan/yifei.wan/AH_Project/PGx_App/Bucket_log.txt) 2> >(tee -a /home/yifei.wan/yifei.wan/AH_Project/PGx_App/Bucket_err_log.txt >&2)
 }
 
 
 ## Run
 {
 	## try to move action files
+	echo $date >> /home/yifei.wan/yifei.wan/AH_Project/PGx_App/Bucket_log.txt
+	echo $date >> /home/yifei.wan/yifei.wan/AH_Project/PGx_App/Bucket_err_log.txt
 	Move_flat $date
 } || { ## sleep and try agian
 	echo Hold 5 seconds!
@@ -49,4 +64,5 @@ Move_flat() {
 	Move_flat
 }
 
-
+## Clear the source folder:
+rm -r $ACTIONFOLDER/*.txt
