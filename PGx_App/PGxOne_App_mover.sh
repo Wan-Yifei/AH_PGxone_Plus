28d3d55bc425d0983cda8ff0b818773219ca2eb2
Wan-Yifei <yifei.wan@admerahealth.com>
2019-04-19 12:22:17 -0400

    Feat: Upload action files in the resolved folder File: PGxOne_App_mover.sh
    Upload action files in the resolved folder and clear the folder.
    Upgrade version number to Beta V0.2.0.

--- PGx_App/PGxOne_App_mover.sh
+++ PGx_App/PGxOne_App_mover.sh
@@ -4,6 +4,7 @@
 
 # ======================================================================
 # 04/02/2019	Basic version 0.0.1
+# Feat: 
 # Copy action files from spcified LIS folder to PGxOne_App folder.
 #
 # **Input Argument**
@@ -17,6 +18,13 @@
 # 2. Add batch uploading;
 # 3. Redirect standard output and error message to both console and file.
 # ======================================================================
+# 04/19/2019	Basic version 0.1.0
+# Feat: 
+# Check and upload resolved action files
+#
+# 1. move action files in the resolved folder;
+# 2. clear files in the resolved folder.
+# ======================================================================
 
 set -e
 
@@ -24,10 +32,13 @@ RUNFOLDER=$1
 LISFOLDER=/data/CLIA-Data/PGxOne_V3/Production/BI_Data_Analysis/$RUNFOLDER/LIS
 APPFOLDER='/data/CLIA-Data/PGxOne_APP'
 BATCHUPLOAD='/xifin/result-data/'
+RELEASE='/data/CLIA-Data/PGx_Resolved/Release'
 
 echo >> PGxOne_App_log.txt
 echo >> PGxOne_UPLOAD_log.txt
 cp -v $LISFOLDER/*.txt $APPFOLDER/ > >(tee -a /home/yifei.wan/AH_Project/PGx_App/PGxOne_APP_log.log) 2> >(tee -a /home/yifei.wan/AH_Project/PGx_App/PGxOne_APP_err.log >&2)
+cp -v $RELEASE/*.txt $APPFOLDER/ > >(tee -a /home/yifei.wan/AH_Project/PGx_App/PGxOne_APP_log.log) 2> >(tee -a /home/yifei.wan/AH_Project/PGx_App/PGxOne_APP_err.log >&2)
 read -p "Input any key to initial batch uploading:"
 cp -v $LISFOLDER/*.txt $BATCHUPLOAD/ > >(tee -a /home/yifei.wan/AH_Project/PGx_App/PGxOne_UPLOAD_log.txt) 2> >(tee -a /home/yifei.wan/AH_Project/PGxOne_UPLOAD_log.txt >&2)
-
+cp -v $RELEASE/*.txt $BATCHUPLOAD/ > >(tee -a /home/yifei.wan/AH_Project/PGx_App/PGxOne_UPLOAD_log.txt) 2> >(tee -a /home/yifei.wan/AH_Project/PGxOne_UPLOAD_log.txt >&2)
+rm -v  $RELEASE/*.txt
