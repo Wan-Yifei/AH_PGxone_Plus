8b1eae20336c70fcb857a3ee3d3fa04c8917e6f1
Wan-Yifei <yifei.wan@admerahealth.com>
2019-05-06 17:57:58 -0400

    Feat: Integrate deleting, checking tags and uploading File: PGx_reporting_pipeline.sh
    Integrate three scripts.

---
+++ PGx_QC/PGx_reporting_pipeline.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+# @author: Yifei Wan
+
+# ==========================================================================
+# 05/06/2019
+# 
+# This pipeline accepts the name of run folder then deleles failed sample an# d un-resolved samples. Finally it would upload actions files to xifin.
+# ==========================================================================
+
+set -e
+
+RUNFOLDER=$1
+QC_list=$2
+python3 /home/yifei.wan/AH_Project/PGx_QC/PGx_Delete.py $RUNFOLDER $QC_list
+
+python /data/AmendReports/Script/check_release.py $RUNFOLDER
+
+bash /home/yifei.wan/AH_Project/PGx_App/PGxOne_App_mover.sh $RUNFOLDER
+
