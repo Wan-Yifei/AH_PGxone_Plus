84ef81e0ed8fa610956f67a34ace9db13fab080e
Wan-Yifei <yifei.wan@admerahealth.com>
2019-05-20 18:05:36 -0400

    Feat: Use original IDs File: PGx_NY.py
    1. Use requesition IDs rather than case IDs;
    2. Change user group after copying.

--- PGx_NY/PGx_NY.py
+++ PGx_NY/PGx_NY.py
@@ -12,7 +12,7 @@ import sys
 runfolder = sys.argv[1]
 
 accession_file = '/data/AmendReports/Resolved_list/CSVFile.csv'
-samples_ids = '/data/CLIA-Data/PGxOne_V3/Production/BI_Data_Analysis/%s/sample_codes_drugs.txt' %runfolder
+samples_ids = '/data/CLIA-Data/PGxOne_V3/Production/BI_Data_Analysis/%s/sample_codes_drugs.txt' % runfolder
 
 output_log = '/data/CLIA-Data/PGxOne_NYHotel/logs/'
 output_folder = '/data/CLIA-Data/PGxOne_NYHotel/'
@@ -24,15 +24,23 @@ if not os.access(accession_file, os.F_OK):
 # Identify NY samples
 with open(samples_ids, 'r') as ids, open(accession_file, 'r') as accession:
     case_ids = [case.strip().split('\t')[3] for case in ids.readlines()]
-    NY_ids = ([sample.strip().split(',')[2] for sample in accession.readlines()
+    NY_ids = ({sample.strip().split(',')[2] : sample.strip().split(',')[3] for sample in accession.readlines()
                 if sample.strip().split(',')[2] in case_ids and 
-                'NY Hotel' in sample.strip().split(',')[1]])
+                'NY Hotel' in sample.strip().split(',')[1]})
+
+if not NY_ids:
+    print ' Cannot find any NY samples!'
+    quit()
 
 with open(output_log + 'processed_log.txt', 'a+') as logging, open(output_log + 'NY_list.txt', 'a+') as id_list:
     ids = id_list.readlines()
-    for action in NY_ids:
-        print 'Copy ' + action
-        os.system('cp %s/LIS/%s.txt %s' %('/data/CLIA-Data/PGxOne_V3/Production/BI_Data_Analysis/' + runfolder, action, output_folder))
-        print >> logging, action
+    for action in NY_ids.keys():
+        print 'Copy ' + action + '/' + NY_ids[action]
+        os.system('cp /data/CLIA-Data/PGxOne_V3/Production/BI_Data_Analysis/%s/LIS/%s.txt %s' 
+        % (runfolder, action, output_folder + '/' + NY_ids[action] + '.txt'))
+        ## chage user group
+        os.system('chgrp CLIA /data/CLIA-Data/PGxOne_V3/Production/BI_Data_Analysis/%s/LIS/%s.txt %s' 
+        % (runfolder, action, output_folder + '/' + NY_ids[action] + '.txt'))
+        print >> logging, action + '/' + NY_ids[action]
         if action + '\n' not in ids:
             print >> id_list, action
