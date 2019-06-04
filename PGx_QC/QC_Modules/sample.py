09f1f4b068002dae7a8947e0fda9ecbb2d4105c1
Wan-Yifei <yifei.wan@admerahealth.com>
2019-06-04 19:18:28 -0400

    Fix: Debugging File: sample.py
    1. change int() to float() of median coverage;
    2. reverse the logical flag of low coverage amplicom QC result.

--- PGx_QC/QC_Modules/sample.py
+++ PGx_QC/QC_Modules/sample.py
@@ -26,13 +26,13 @@ class Sample(object):
             self.failed_critical_amp, self.failed_amp_notice, self.QC_amplicon_pass = self.amp_less_5_QC(Drug_action, Range)
             ## dict of Gene object having socre info with coverage less than 5 
             self.failed_critical_scored_amp, self.failed_scored_amp_notice, self.scored_QC = self.scored_less_5_QC(Output_geno, Active_score, Drug_action, Low_coverage, Range, Gene_KB)        
-        self.Check()
 
-    def Check(self):
+    def check(self):
         print 'Sample ID: %s'%self.ID
         print 'Sample ICDs: %s'%self.ICD
-        print 'Coverage less than 5: %s'%self.low_count_ex_amplicon
-        print 'Scored amplicons having coverage less than 5: %s'%self.low_count_scored_amplicon
+        if self.QC_complete_pass:
+            print 'Coverage less than 5: %s'%self.low_count_ex_amplicon.keys()
+            print 'Scored amplicons having coverage less than 5: %s'%self.low_count_scored_amplicon
         print 'Gobal Pass: %s'%self.QC_complete_pass
         print 'CYP2D6 global QC status note: %s'%self.QC_CYP2D6_notice
         try:
@@ -44,7 +44,7 @@ class Sample(object):
             print self.failed_scored_amp_notice
             print 'Scored amplicon QC pass: %s'%self.scored_QC
         except:
-            print 'No scored objects generated!'
+            print 'No scored instances generated!'
 
     def get_ICD(self, Code_drug):
         ICDs = next(ICD.strip().split('\t')[1].split(', ') for ICD in Code_drug if self.ID in ICD)
@@ -53,7 +53,7 @@ class Sample(object):
         
     def get_low_count_amp(self, Low_coverage):
         ## key: amplicon_name, value: count
-        low_amplicons = {amp.strip().split('\t')[2] : int(amp.strip().split('\t')[3]) for amp in Low_coverage if self.ID in amp} 
+        low_amplicons = {amp.strip().split('\t')[2] : float(amp.strip().split('\t')[3]) for amp in Low_coverage if self.ID in amp} 
         ## key: amplicon_name, value: gene_name
         gene_amplicons = {amp.strip().split('\t')[2] : amp.strip().split('\t')[1].split('_')[0] for amp in Low_coverage if self.ID in amp}  
         ## amplicons with coverage less than 6. key: amplicon_name, value: gene_name
@@ -99,7 +99,6 @@ class Sample(object):
         for amp in self.low_count_ex_amplicon.keys():
            values = low_ex_gene.get(amp, [])
            low_ex_gene[self.low_count_ex_amplicon[amp]] = values + [amp] ## key: gene, values: amplicons
-
         if len(self.low_count_ex_amplicon) > 7:
              QC_status = False ## too many critial amplicons have extrme low coverage
              failed_amp_notice = '%s critical amplicons have coverage less than 6, sample has completely failed!'%len(low_count_ex_amplicon)
@@ -107,7 +106,7 @@ class Sample(object):
              amplicon_check = {low_gene[0] : G.Gene(self.ID, self.ICD, low_gene[0], Drug_action, low_gene[1], Range) for low_gene in low_ex_gene.items()}
         failed_gene = [gene[0] for gene in amplicon_check.items() if gene[1].ICD_relevant and gene[0]]
         potential_failed_gene = [gene for gene in failed_gene if gene not in self.AMPLICONS_SCORED_LIST]
-        QC_status = bool(potential_failed_gene)
+        QC_status = not bool(potential_failed_gene) #True: passed; False: failed
         if QC_status:
             failed_amp_notice = 'Sample %s has failed on critical amplicons: %s'%(self.ID, failed_gene)
         else:
