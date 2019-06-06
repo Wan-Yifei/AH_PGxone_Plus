bf6151d33d50d0083000b5018735e401b7656f5c
Wan-Yifei <yifei.wan@admerahealth.com>
2019-06-06 16:32:08 -0400

    Ref: Change the group of files File: PGxOne_Scripts.sh
    Introduce the change_group.py to set group of files.

--- PGx_Run/PGxOne_Scripts.sh
+++ PGx_Run/PGxOne_Scripts.sh
@@ -16,10 +16,5 @@ echo "5. Cleanup done!"
 python PGxOneV3_Drug_DDI.py
 echo "6. DDI done!"
 
-#echo ====================================================
-#chgrp -c CLIA /data/CLIA-Data/PGxOne_V3/Production/BI_Data_Analysis/$1/LIS/*
-#chgrp -c CLIA /data/CLIA-Data/PGxOne_V3/Production/BI_Data_Analysis/$1/LIS/*
-#chgrp -c CLIA /data/CLIA-Data/PGxOne_V3/Production/BI_Data_Analysis/$1/DDI/*
-#echo ====================================================
-#echo
-#echo The groups of all files has been changed to CLIA!
+python3 /home/yifei.wan/PGx_Run/change_group.py $1
+echo The groups of all files has been changed to CLIA!
