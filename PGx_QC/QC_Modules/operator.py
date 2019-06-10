145250a1e3120631fe4a4e19902ed7674b0f542d
Wan-Yifei <yifei.wan@admerahealth.com>
2019-06-10 00:38:20 -0400

    Feat: pasre "==" File: operator.py

--- PGx_QC/QC_Modules/operator.py
+++ PGx_QC/QC_Modules/operator.py
@@ -3,12 +3,14 @@ import sys
 import re
 
 a = sys.argv[1]
-string = 'poor: <=0.75; intermediate: >0.75 & <=1.25; normal: >1.25 & <= 2.5; High inducibility: >2.5'
+#string = 'poor: <=0.75; intermediate: >0.75 & <=1.25; normal: >1.25 & <= 2.5; High inducibility: >2.5'
+string = 'slow: 0; intermediate: 1; rapid: 2'
 
 parsescore = {'<': operator.lt,
             '>': operator.gt,
             '>=': operator.ge,
-            '<=': operator.le}
+            '<=': operator.le,
+            '==': operator.eq }
 
 
 operator_pattern = re.compile(r'(<|>|>=|<=)')
@@ -20,8 +22,10 @@ a = float(a)
 for val in level.values():
     compare = operator_pattern.findall(val)
     num = num_pattern.findall(val)
-    #print val
-    #print compare
+    if not compare:
+        compare = ['==' for opt in num]
+    print val
+    print compare
     #print num
     result = [parsescore[opt](a, float(num[compare.index(opt)])) for opt in compare]
     for opt in compare:
